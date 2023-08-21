c2961168a866f30ae2428b901d8c9667c7dbea15
Stefan Alexandru <stefanalecsandru@yahoo.com>
2023-08-21 16:02:11 +0300

    push changes

---
+++ index.js
@@ -0,0 +1,37 @@
+const inputField = document.getElementById('input');
+const itemsList = document.getElementById('to-do-list');
+let counter = 1;
+
+function addListItem() {
+    const newItem = document.createElement('li');
+    const trimmedValue = inputField.value.trim();
+
+    if (trimmedValue === '') {
+        return;
+    }
+    newItem.textContent = trimmedValue;
+    
+    newItem.addEventListener('click', function () {
+        if (newItem.style.textDecoration !== 'line-through') {
+            newItem.style.textDecoration = 'line-through';
+        }
+        else {
+            newItem.style.textDecoration = 'none';
+        }
+    });
+
+    newItem.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+        itemsList.removeChild(newItem);
+    });
+    
+    itemsList.appendChild(newItem);
+    counter++;
+    inputField.value = '';
+}
+
+inputField.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        addListItem();
+    }
+});
