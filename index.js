33dca9a5984af7ff97d458cafa87c7bfe405e6ad
Stefan Alexandru <stefanalecsandru@yahoo.com>
2023-08-26 21:48:16 +0300

    update the code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputField = document.getElementById('input');
 const itemsList = document.getElementById('to-do-list');
 let counter = 1;
+const savedItems = JSON.parse(localStorage.getItem('todoItems')) || [];
 
 function addListItem() {
     const newItem = document.createElement('li');
@@ -10,25 +11,54 @@ function addListItem() {
         return;
     }
     newItem.textContent = trimmedValue;
-    
+
     newItem.addEventListener('click', function () {
         newItem.style.textDecoration !== 'line-through' ?
             newItem.style.textDecoration = 'line-through' :
             newItem.style.textDecoration = 'none';
+        saveToLocalStorage();
+        
     });
+    savedItems.push(newItem.textContent);
 
     newItem.addEventListener("contextmenu", function (e) {
         e.preventDefault();
         itemsList.removeChild(newItem);
+        savedItems.pop();
+
+        saveToLocalStorage();
     });
-    
     itemsList.appendChild(newItem);
+    saveToLocalStorage();
     counter++;
     inputField.value = '';
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('todoItems', JSON.stringify(savedItems));
 }
 
 inputField.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         addListItem();
+        console.log(savedItems);
     }
+});
+
+savedItems.forEach(itemText => {
+    const newItem = document.createElement('li');
+    newItem.textContent = itemText;
+    newItem.addEventListener('click', function () {
+        newItem.style.textDecoration = newItem.style.textDecoration !== 'line-through' ?
+            'line-through' :
+            'none';
+        saveToLocalStorage();
+    });
+    newItem.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+        itemsList.removeChild(newItem);
+        savedItems.pop();
+        saveToLocalStorage();
+    });
+    itemsList.appendChild(newItem);
 });
